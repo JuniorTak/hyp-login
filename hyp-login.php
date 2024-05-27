82ba301d242c7a2780d9b64b322a7f1ef882ed25
Hyppolite <hyppolitak@gmail.com>
2024-05-27 21:49:06 +0100

    Define the plugin header

---
+++ hyp-login.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * @since             1.0.0
+ * @package           Hyp_Login
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Hyp Login
+ * Plugin URI:        https://github.com/JuniorTak
+ * Description:       Custom Login UI
+ * Version:           1.0.0
+ * Author:            Hyppolite T.
+ * Author URI:        https://www.hyppolitetakouafoduop.online
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       hyp-login
+ * Domain Path:       /languages
+ */
